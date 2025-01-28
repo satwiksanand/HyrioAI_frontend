7fb9f60ef21c4b03d893dd9b0b4bf8de43315796
satwik sanand <sanandsatwik421@gmail.com>
2025-01-28 10:28:27 +0000

    vercel deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import AppLayout from "./components/AppLayout";
 import PrivateRoute from "./components/PrivateRoute";
 import SignIn from "./Pages/SignIn";
@@ -7,18 +6,16 @@ import SignUp from "./Pages/SignUp";
 import Home from "./Pages/Home";
 import VerifyEmail from "./Pages/VerifyEmail";
 import VerifyMobile from "./Pages/VerifyMobile";
-import { ToastContainer } from "react-toastify";
 import CreateJob from "./Pages/CreateJob";
+import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
       {
-        //this is the default route and if the user is logged in it will show up or signup sir.
         element: <PrivateRoute />,
         children: [
-          //here we will have components that we will show after signin
           {
             path: "/",
             element: <Home />,
